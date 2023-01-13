d289d27654a955f81f8341ea0307bba697421713
Badar Wildani <wildanie12@gmail.com>
2023-01-13 21:33:19 +0700

    add: font and color theme extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        podkova: ["Podkova", "sans-serif"],
+        poppins: ["Poppins", "sand-serif"]
+      },
+      colors: {
+        wdSky: "#31afd4",
+        wdBlue: "#08415c",
+        wdYellow: "#f7b32b",
+        wdRed: "#ff1053",
+        wdDark: "#0e171b"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
