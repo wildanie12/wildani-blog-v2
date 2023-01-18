a16cfa5468ce737bc1391d7cffdb53d73a754df8
Badar Wildani <wildanie12@gmail.com>
2023-01-18 21:33:19 +0700

    add: container screens breakpoint adjustments

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
   darkMode: "class",
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+      screens: {
+        xs: "100%",
+        sm: "100%",
+        md: "600px",
+        lg: "768px",
+        xl: "1000px",
+        "2xl": "1200px"
+      }
+    },
     extend: {
       fontFamily: {
         podkova: ["Podkova", "sans-serif"],
