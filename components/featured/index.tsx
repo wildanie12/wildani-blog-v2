35e6556dba1c56d3e4b972f170557f3c2af4a67b
Badar Wildani <wildanie12@gmail.com>
2023-01-20 21:33:19 +0700

    fix: lg screen layout shift

--- components/featured/index.tsx
+++ components/featured/index.tsx
@@ -8,8 +8,8 @@ type FeaturedProps = {
 export default function Featured({ className = "" }: FeaturedProps): JSX.Element {
   return (
     <div className={`dark:bg-wdDark2 dark:bg-opacity-10 dark:backdrop-blur-lg bg-white hidden lg:flex rounded-3xl shadow-xl gap-8 p-7 ${className}`}>
-      <BigPost className="2xl:w-3/4 lg:w-10/12" />
-      <div className="g:2/12 2xl:w-1/4 flex flex-col gap-4">
+      <BigPost className="2xl:w-9/12 lg:w-8/12 shrink-0" />
+      <div className="grow-0 2xl:w-3/12 lg:3/12 flex flex-col gap-4">
         <FeaturedPost />
         <FeaturedPost />
       </div>
