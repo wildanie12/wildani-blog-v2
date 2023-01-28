461aedab133ec0597c38306ebc65a5d3b74c1656
Badar Wildani <wildanie12@gmail.com>
2023-01-28 21:33:19 +0700

    feat: second level featured post component

--- components/featured/FeaturedPost.tsx
+++ components/featured/FeaturedPost.tsx
@@ -1,21 +1,38 @@
+import moment from "moment"
 import Image from "next/image"
+import Link from "next/link"
 
 type FeaturedPostProps = {
   className?: string
+  post: IPost
 }
 
-export default function FeaturedPost({ className }: FeaturedPostProps): JSX.Element {
+export default function FeaturedPost({ className, post }: FeaturedPostProps): JSX.Element {
   return (
-    <div className={` ${className}`}>
+    <Link href="/posts/[slug]" as={`/posts/${post.attributes.slug}`} className={`group ${className}`}>
       <div className="w-full rounded-3xl relative aspect-video overflow-hidden">
-        <Image src="/sample/post-banner.jpg" alt="featured big post banner" fill={true} style={{ objectFit: "cover" }} />
+        <Image
+          src={`${process.env.NEXT_PUBLIC_ASSET_URL}${post.attributes.banner.data.attributes.url}`}
+          alt="featured big post banner"
+          fill={true}
+          style={{ objectFit: "cover" }}
+        />
       </div>
-      <div className="flex flex-col justify-start items-start font-poppins mt-2">
-        <div className="flex gap-2 text-gray-400">
-          <div className="text-xs font-medium uppercase tracking-widest">July 28, 2022</div>
+      <div className="flex items-stretch">
+        <div className="w-0 group-hover:w-2 my-2 group-hover:mr-4 transition-all ease-out duration-200 bg-wdRed"></div>
+
+        <div className="flex flex-col justify-start items-start font-poppins mt-2">
+          <div className="flex gap-2 text-gray-400">
+            <div className="text-xs font-medium uppercase tracking-widest">{moment(post.attributes.createdAt).startOf("days").fromNow()}</div>
+          </div>
+          <h2 className="text-wdBlue text-lg font-semibold dark:text-wdDarkSky">{post.attributes.title}</h2>
+          <p className="dark:text-gray-300 text-sm">
+            {post.attributes.excerpt.slice(0, 60)}
+            {post.attributes.excerpt.length > 60 && " . . ."}
+          </p>
         </div>
-        <h2 className="text-wdBlue text-lg font-semibold dark:text-wdDarkSky">ChartJS dan Implementasinya menggunakan next.js</h2>
+        <div className="w-2 group-hover:w-0 my-2 shrink-0 ml-4 transition-all duration-200"></div>
       </div>
-    </div>
+    </Link>
   )
 }
