d6c181da4f99d1686cd81d9f4579a405f0819f4d
Badar Wildani <wildanie12@gmail.com>
2023-01-29 21:33:19 +0700

    feat: featured post component

--- components/featured/index.tsx
+++ components/featured/index.tsx
@@ -3,17 +3,20 @@ import FeaturedPost from "./FeaturedPost"
 
 type FeaturedProps = {
   className?: string
+  posts: IPost[]
 }
 
-export default function Featured({ className = "" }: FeaturedProps): JSX.Element {
+export default function Featured({ className = "", posts = [] }: FeaturedProps): JSX.Element {
+  if (posts.length == 0) {
+    return <></>
+  }
   return (
-    <div className={`dark:bg-wdDark2 dark:bg-opacity-10 dark:backdrop-blur-lg bg-white hidden lg:flex rounded-3xl shadow-xl gap-8 p-7 ${className}`}>
-      <BigPost className="2xl:w-9/12 lg:w-8/12 shrink-0" />
-      <div className="grow-0 2xl:w-3/12 lg:3/12 flex flex-col gap-4">
-        <FeaturedPost />
-        <FeaturedPost />
+    <div className={`dark:bg-wdDark2 dark:bg-opacity-10 dark:backdrop-blur-lg bg-white flex rounded-3xl shadow-xl gap-8 p-7 ${className}`}>
+      <BigPost post={posts[0]} className="2xl:w-9/12 lg:w-8/12 w-full shrink-0" />
+      <div className="hidden justify-start lg:flex grow-0 2xl:w-3/12 lg:3/12 flex-col gap-4">
+        {typeof posts[1] !== "undefined" && <FeaturedPost post={posts[1]} />}
+        {typeof posts[2] !== "undefined" && <FeaturedPost post={posts[2]} />}
       </div>
-      {/* <BigPost className="flex-grow" /> */}
     </div>
   )
 }
