efc668c060720f512ef1bbeccec207a6b04be92f
Badar Wildani <wildanie12@gmail.com>
2023-02-04 21:33:19 +0700

    add: sample post tags

---
+++ constants/tag.ts
@@ -0,0 +1,56 @@
+import { TagData } from "../components/sidebar/Tags"
+
+export const tags: TagData[] = [
+  {
+    name: "#javascript",
+    link: "/tags/javascript"
+  },
+  {
+    name: "#typescript",
+    link: "/tags/typescript"
+  },
+  {
+    name: "#golang",
+    link: "/tags/golang"
+  },
+  {
+    name: "#css",
+    link: "/tags/css"
+  },
+  {
+    name: "#html",
+    link: "/tags/html"
+  },
+  {
+    name: "#ruby",
+    link: "/tags/ruby"
+  },
+  {
+    name: "#system-design",
+    link: "/tags/system-design"
+  },
+  {
+    name: "#microservices",
+    link: "/tags/microservices"
+  },
+  {
+    name: "#monorepo",
+    link: "/tags/monorepo"
+  },
+  {
+    name: "#clean-code",
+    link: "/tags/clean-code"
+  },
+  {
+    name: "#ci-cd",
+    link: "/tags/ci-cd"
+  },
+  {
+    name: "#jenkins",
+    link: "/tags/jenkins"
+  },
+  {
+    name: "#docker",
+    link: "/tags/docker"
+  }
+]
