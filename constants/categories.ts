9e2a228ff5c1bbb2929f4890c9f0d1fd4c57fcbd
Badar Wildani <wildanie12@gmail.com>
2023-02-04 21:33:19 +0700

    add: sample categories

---
+++ constants/categories.ts
@@ -0,0 +1,24 @@
+import { CategoryData } from "../components/sidebar/Categories"
+
+export const categories: CategoryData[] = [
+  {
+    name: "Back-end",
+    image: "/sample/categories/backend.jpg",
+    link: "/categories/back-end"
+  },
+  {
+    name: "Front-end",
+    image: "/sample/categories/frontend.jpg",
+    link: "/categories/front-end"
+  },
+  {
+    name: "System Design",
+    image: "/sample/categories/system-design.jpg",
+    link: "/categories/system-design"
+  },
+  {
+    name: "Internet of Things",
+    image: "/sample/categories/internet-of-things.jpg",
+    link: "/categories/internet-of-things"
+  }
+]
