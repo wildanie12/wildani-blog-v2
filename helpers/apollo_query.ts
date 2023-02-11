6414af9f4a3534ca3dfd82c0ef81ebc8de4baa1d
Badar Wildani <wildanie12@gmail.com>
2023-02-11 21:33:19 +0700

    add: get posts by category gql query

--- helpers/apollo_query.ts
+++ helpers/apollo_query.ts
@@ -106,3 +106,59 @@ export const GET_SINGLE_POST = gql`
   }
 `
 
+export const GET_POSTS_BY_CATEGORY = gql`
+  query getPostsByCategory($slug: String) {
+    posts(filters: { category: { slug: { eq: $slug } } }) {
+      data {
+        id
+        attributes {
+          title
+          read_time
+          banner {
+            data {
+              id
+              attributes {
+                name
+                url
+              }
+            }
+          }
+          excerpt
+          body
+          table_of_contents
+          seo_description
+          slug
+          category {
+            data {
+              id
+              attributes {
+                title
+              }
+            }
+          }
+          tags {
+            data {
+              id
+              attributes {
+                name
+                icon_svg
+              }
+            }
+          }
+          createdAt
+          updatedAt
+          publishedAt
+        }
+      }
+      meta {
+        pagination {
+          total
+          page
+          pageSize
+          pageCount
+        }
+      }
+    }
+  }
+`
+
