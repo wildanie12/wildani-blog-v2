fc9dafd49d4175d999d6048a60640cf59c9a74ad
Badar Wildani <wildanie12@gmail.com>
2023-02-11 21:33:19 +0700

    add: get single post gql query

--- helpers/apollo_query.ts
+++ helpers/apollo_query.ts
@@ -56,3 +56,53 @@ export const GET_LATEST_POST = gql`
   }
 `
 
+export const GET_SINGLE_POST = gql`
+  query getSinglePost($slug: String) {
+    posts(filters: { slug: { eq: $slug } }) {
+      data {
+        id
+        attributes {
+          title
+          read_time
+          banner {
+            data {
+              id
+              attributes {
+                name
+                url
+              }
+            }
+          }
+          excerpt
+          body
+          table_of_contents
+          seo_description
+          slug
+          category {
+            data {
+              id
+              attributes {
+                title
+              }
+            }
+          }
+          tags {
+            data {
+              id
+              attributes {
+                name
+                icon_svg
+                slug
+              }
+            }
+          }
+          epilogue
+          createdAt
+          updatedAt
+          publishedAt
+        }
+      }
+    }
+  }
+`
+
