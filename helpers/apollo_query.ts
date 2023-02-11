338498e26bd11eb7b3be8d83839b506e5545bff7
Badar Wildani <wildanie12@gmail.com>
2023-02-11 21:33:19 +0700

    add: get latest post gql query

---
+++ helpers/apollo_query.ts
@@ -0,0 +1,58 @@
+import { gql } from "@apollo/client"
+
+export const GET_LATEST_POST = gql`
+  query latestPosts($page: Int, $pageSize: Int) {
+    posts(pagination: { page: $page, pageSize: $pageSize }) {
+      data {
+        id
+        attributes {
+          title
+          read_time
+          banner {
+            data {
+              id
+              attributes {
+                name
+                url
+              }
+            }
+          }
+          excerpt
+          body
+          table_of_contents
+          seo_description
+          slug
+          category {
+            data {
+              id
+              attributes {
+                title
+              }
+            }
+          }
+          tags {
+            data {
+              id
+              attributes {
+                name
+                icon_svg
+              }
+            }
+          }
+          createdAt
+          updatedAt
+          publishedAt
+        }
+      }
+      meta {
+        pagination {
+          total
+          page
+          pageSize
+          pageCount
+        }
+      }
+    }
+  }
+`
+
