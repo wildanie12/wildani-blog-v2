3e9beab304f8ed6eef71d4d348fdce7b58563cb7
Badar Wildani <wildanie12@gmail.com>
2023-02-18 03:07:56 +0700

    fix: temporary unoptimized images

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost", "180.247.49.39", "103.150.196.156"]
+    domains: ["localhost", "180.247.49.39", "103.150.196.156"],
+    unoptimized: true
   }
 }
