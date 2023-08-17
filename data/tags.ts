6e6999df87ecdd28c33a26c2a2f86a6b159035dc
Badar Wildani <wildanie12@gmail.com>
2023-08-17 16:24:19 +0700

    add: tags data

---
+++ data/tags.ts
@@ -0,0 +1,56 @@
+export const tags: { [key: string]: ITag } = {
+  typescript: {
+    id: 1,
+    attributes: {
+      name: "Typescript",
+      slug: "typescript",
+      description: "typescript topics",
+      icon_svg: "/sample/tags/typescript.svg"
+    }
+  },
+  rabbitmq: {
+    id: 1,
+    attributes: {
+      name: "rabbitmq",
+      slug: "rabbitmq",
+      description: "rabbitmq topics",
+      icon_svg: "/sample/tags/rabbitmq.svg"
+    }
+  },
+  docker: {
+    id: 1,
+    attributes: {
+      name: "docker",
+      slug: "docker",
+      description: "docker topics",
+      icon_svg: "/sample/tags/docker.svg"
+    }
+  },
+  mongodb: {
+    id: 1,
+    attributes: {
+      name: "mongodb",
+      slug: "mongodb",
+      description: "mongodb topics",
+      icon_svg: "/sample/tags/mongodb.svg"
+    }
+  },
+  golang: {
+    id: 1,
+    attributes: {
+      name: "golang",
+      slug: "golang",
+      description: "golang topics",
+      icon_svg: "/sample/tags/golang.svg"
+    }
+  },
+  websocket: {
+    id: 1,
+    attributes: {
+      name: "websocket",
+      slug: "websocket",
+      description: "websocket topics",
+      icon_svg: "/sample/tags/websocket.svg"
+    }
+  }
+}
