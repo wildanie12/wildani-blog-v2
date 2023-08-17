86d7d7a514b02ae06048808536e2c17360938136
Badar Wildani <wildanie12@gmail.com>
2023-08-17 16:24:19 +0700

    add: uploads bucket data

---
+++ data/uploads.ts
@@ -0,0 +1,40 @@
+export const categoryImages: { [key: string]: IUpload } = {
+  "backend.jpg": {
+    id: 1,
+    attributes: {
+      name: "backend.jpg",
+      url: "/sample/categories/backend.jpg"
+    }
+  },
+  "system-design.jpg": {
+    id: 1,
+    attributes: {
+      name: "system-design.jpg",
+      url: "/sample/categories/system-design.jpg"
+    }
+  },
+  "internet-of-things.jpg": {
+    id: 1,
+    attributes: {
+      name: "internet-of-things.jpg",
+      url: "/sample/categories/internet-of-things.jpg"
+    }
+  },
+  "frontend.jpg": {
+    id: 1,
+    attributes: {
+      name: "frontend.jpg",
+      url: "/sample/categories/frontend.jpg"
+    }
+  }
+}
+
+export const postImages: { [key: string]: IUpload } = {
+  "1-banner.jpg": {
+    id: 1,
+    attributes: {
+      name: "chart-js.jpg",
+      url: "/sample/posts/1/Cover 2.png"
+    }
+  }
+}
